4aeff010e7486a8577d2b2df9002ea4f2809115a
simonpai <simonpai@potix.com>
2013-04-12 17:46:14 +0800

    dart files skeleton.

--- lib/bootjack.dart
+++ lib/bootjack.dart
@@ -1,3 +1,17 @@
 library bootjack;
 
 import 'dart:html';
+
+part 'src/affix.dart';
+part 'src/alert.dart';
+part 'src/button.dart';
+part 'src/carousel.dart';
+part 'src/collapse.dart';
+part 'src/dropdown.dart';
+part 'src/modal.dart';
+part 'src/popover.dart';
+part 'src/scrollspy.dart';
+part 'src/tab.dart';
+part 'src/tooltip.dart';
+part 'src/transition.dart';
+part 'src/typeahead.dart';
